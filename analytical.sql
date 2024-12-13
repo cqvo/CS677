7360e2b13a80febc2415e90080f7996445430c7f
Chris Vo <50410061+cqvo@users.noreply.github.com>
2024-12-12 20:44:09 -0500

    Update analytical.sql

--- analytical.sql
+++ analytical.sql
@@ -1,24 +1,105 @@
--- Query to find the number of clients with active items
-select count(distinct client_id) as active_clients_count
-from public.dim_items
-where is_current = true;
+-- Similar to link requests, this table records the fact of when a request has been made
+-- We also record when the request has been closed out with completed_at, making it bi-temporal
+create table public.fact_report_requests
+(
+    id serial
+        constraint fact_report_requests_pk
+            primary key,
+    plaid_id varchar not null
+        constraint fact_report_requests_pk_2
+            unique,
+    item_id int not null
+        constraint fact_report_requests_fk_dim_items
+            references public.dim_items (id),
+    completed_at timestamp default current_timestamp,
+    created_at timestamp default current_timestamp not null,
+    updated_at timestamp default current_timestamp not null,
+    report_token varchar not null
+        constraint fact_report_requests_pk_3
+            unique
+);
 
--- Query to find institutions with the most accounts
-select di.current_name, count(da.id) as account_count
-from public.dim_institutions di
-join public.dim_items dii on di.id = dii.institution_id
-join public.dim_accounts da on dii.id = da.item_id
-group by di.id, di.plaid_institution_id, di.current_name
-order by account_count desc
-limit 10;
+-- We receive the raw json back from Plaid and store it in this staging table
+create table public.staging_report
+(
+    id serial
+        constraint staging_report_pk
+            primary key,
+    created_at timestamp default current_timestamp not null,
+    data jsonb not null
+);
 
--- Query to find items due for key rotation
-select di.id, di.key_name
-from public.dim_items di
-where di.updated_at < current_date - interval '30 days';
-
--- Query to find average age of items, i.e. how good are we at staying on top of rotation?
+-- We can flatten the json enough to be in a much more usable format
 select
-    avg(current_date - updated_at) as average_item_age_in_days
-from
-    public.dim_items;
+    data->'report'->>'asset_report_id' as asset_report_id,
+    data->'report'->>'client_report_id' as client_report_id,
+    cast(data->'report'->>'date_generated' as timestamp) as created_at,
+    account->>'account_id' as account_id,
+    account->'transactions' as transactions,
+    account->'historical_balances' as historical_balances
+from staging_report
+cross join lateral jsonb_array_elements(data->'report'->'items') as items(item)
+cross join lateral jsonb_array_elements(items.item->'accounts') as account
+where id = 1;
+
+-- And now we need a place to store it
+create table public.dim_reports
+(
+    id serial
+        constraint dim_reports_pk
+            primary key,
+    plaid_id varchar not null,
+    client_report_id varchar not null,
+    report_request_id int not null
+        constraint dim_reports_fact_report_requests
+            references public.fact_report_requests (id),
+    account_id varchar not null,
+    created_at timestamp default current_timestamp not null,
+    transactions jsonb,
+    historical_balances jsonb
+);
+
+-- Flattening and inserting results in 12 new rows from the json originally collected
+insert into public.dim_reports (plaid_id, client_report_id, report_request_id, account_id, created_at, transactions, historical_balances)
+select
+    data->'report'->>'asset_report_id' as plaid_id,
+    data->'report'->>'client_report_id' as client_report_id,
+    1 as report_request_id,
+    account->>'account_id' as account_id,
+    cast(data->'report'->>'date_generated' as timestamp) as created_at,
+    account->'transactions' as transactions,
+    account->'historical_balances' as historical_balances
+from staging_report
+cross join lateral jsonb_array_elements(data->'report'->'items') as items(item)
+cross join lateral jsonb_array_elements(items.item->'accounts') as account
+where id = 1;
+
+-- Get the average transaction amount for each account found in the report
+with expanded_transactions as (
+    select
+        account_id,
+        (jsonb_array_elements(transactions)->>'amount')::numeric as amount
+    from dim_reports
+    where report_request_id = 1
+)
+select
+    account_id,
+    avg(amount) as average_transaction_amount
+from expanded_transactions
+group by account_id;
+
+-- Finding the top 3 accounts by average daily balance
+with expanded_balances as (
+    select
+        account_id,
+        (jsonb_array_elements(historical_balances)->>'current')::numeric as balance
+    from dim_reports
+    where report_request_id = 1
+)
+select
+    account_id,
+    avg(balance) as average_historical_balance
+from expanded_balances
+group by account_id
+order by average_historical_balance desc
+limit 3;
